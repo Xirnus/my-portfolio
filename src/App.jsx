2e3a968400d057c424cc4d09363e0938ea4aeb25
Xirnus <Xernous343@gmail.com>
2026-09-04 11:10:14 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from './components/Hero.jsx'
+
+export default function App() {
+  return (
+    <main>
+      <div>
+        <h1>My Developer Portfolio</h1>
+      </div>
+      <Hero/>
+    </main>
+  );
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+    return (
+        <section>
+            <h1>Hi, I'm a Software Developer</h1>
+            <p>
+                Computer Science graduate specializing in full-stack web applications, system automation, and backend troubleshooting.
+            </p>
+        </section>
+    )
+}
